001a0ec70982a38a96ef9c8adbb866b8cd8f8920
Connor McGrath <connorfrancismcgrath@outlook.com>
2026-06-24 04:03:52 -0400

    Set up prisma

--- note-app-backend/src/index.ts
+++ note-app-backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-// import { PrismaClient } from "./generated/prisma/client.ts";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { env } from "node:process";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "node:process";
 
 
 const app = express();
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
-// const prisma = new PrismaClient({adapter});
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+const prisma = new PrismaClient({adapter});
 
 app.listen(5000, () => {
     console.log("server running on localhost:5000");
