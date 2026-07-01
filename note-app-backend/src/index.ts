9159b4c862db3e3db3e40b9189e4e7045c487d2d
Connor McGrath <connorfrancismcgrath@outlook.com>
2026-07-01 13:42:36 -0400

    set up the prisma client

--- note-app-backend/src/index.ts
+++ note-app-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import prisma from "../prisma/prisma.config.ts";
+
 const app = express();
 
 app.use(express.json());
@@ -10,7 +12,9 @@ app.listen(5000, () => {
     console.log("server running on localhost:5000");
 })
 
-app.get("/test/get", (request, response) => {
-    response.json({ message: "test update again!" });
+app.get("/test/get", async (request, response) => {
 
+    const notes = await prisma.note.findMany();
+
+    response.json(notes);
 })
